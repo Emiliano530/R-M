6481284d9f9eef3a6fda2a554ebc1b93035c6be7
Emiliano530 <emiliano.19070011@itsmotul.edu.mx>
2023-02-24 22:30:04 -0600

    Rick-Morty

--- Rick-Morty/src/components/Personajes.vue
+++ Rick-Morty/src/components/Personajes.vue
@@ -6,7 +6,7 @@ export default {
     return {
       info: [],
       personajes: [],
-      cont:2
+      cont:1
     }
   },
   mounted() {
@@ -17,7 +17,7 @@ export default {
       })
   },
   methods: {
-    pag(num) {
+    pagmas(num) {
       API_URL='https://rickandmortyapi.com/api/character/?page='+num
       console.log(API_URL)
       axios.get(API_URL)
@@ -27,6 +27,17 @@ export default {
         this.personajes = response.data.results;
       })
       this.cont++
+    },
+    pagmenos(num) {
+      API_URL='https://rickandmortyapi.com/api/character/?page='+num
+      console.log(API_URL)
+      axios.get(API_URL)
+      .then((response) => {
+        console.log(response.config)
+        this.info = response.data.info;
+        this.personajes = response.data.results;
+      })
+      this.cont--
     }
   },
 }
@@ -34,10 +45,15 @@ export default {
 
 <template>
   <h2>Hay {{ info.count }} personajes en el programa de Rick & Morty</h2>
-  <button @click="pag(cont)">página {{ cont }}</button>
+
+
+
+  <button @click="pagmenos(cont)">Anterior</button>
+  <a>{{ cont }}</a>
+  <button @click="pagmas(cont)">Siguiente</button>
   <ul>
     <li v-for="p in personajes">
-      <a >{{ p.name }} id:{{ p.id }}</a> 
+      <a>{{ p.name }} id:{{ p.id }}</a>
     </li>
   </ul>
 </template>
